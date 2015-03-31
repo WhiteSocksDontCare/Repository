87b181ba375f1b214177a586a9af717a875809b0
sagawater <alexandre443@hotmail.com>
2015-03-31 15:26:08 -0400

    Ajout de la dll de communication

---
+++ TP3/ChatCommunication/ChatCommunication.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ChatCommunication
+{
+    public class Lobby
+    {
+
+    }
+    public class Profile
+    {
+        string Pseudo;
+        string LastName;
+        string FirstName;
+        byte[] Avatar;
+        int IDRoom;
+        int NbLike;
+        int NbDislike;
+        int NbMessage;
+        int NbDeletedMessage;
+        bool IsConnected;
+    }
+    public class Room
+    {
+        int IDRoom;
+        string Name;
+        string Description;
+        bool IsDeleted;
+        List<Profile> SubscribedUsers;
+        List<Message> Messages;
+    }
+    public class User
+    {
+        string Pseudo;
+        string Password;
+    }
+    public class Message
+    {
+        int IDMessage;
+        string Text;
+        bool IsDeleted;
+        string Pseudo;
+        int IDRoom;
+        int NbLike;
+        int NbDislike;
+    }
+    public class Like
+    {
+        int IDMessage;
+        string Pseudo;
+        bool IsLike;
+    }
+
+}
