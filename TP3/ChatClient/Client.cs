195c06bdc61329886fdd9c9d216d6070e69910fa
WhiteSocksDontCare <guillaume.brulotte@gmail.com>
2015-03-31 10:41:41 -0400

    Code de base pour le tp

---
+++ TP3/ChatClient/Client.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Controls.Primitives;
+
+namespace ChatClient
+{
+    public class StateObject
+    {
+        // Client  socket.
+        public Socket workSocket = null;
+        // Size of receive buffer.
+        public const int bufferSize = 1024;
+        // Receive buffer.
+        public byte[] buffer = new byte[bufferSize];
+        // Received data string.
+        public StringBuilder sb = new StringBuilder();
+    }
+
+    class Client
+    {
+        private const int port = 11000;
+
+        private static readonly ManualResetEvent connectDone = new ManualResetEvent(false);
+        private static readonly ManualResetEvent sendDone = new ManualResetEvent(false);
+        private static readonly ManualResetEvent receiveDone = new ManualResetEvent(false);
+
+        private static String response = String.Empty;
+
+        public static bool StartClient(string name)
+        {
+            try
+            {
+                var ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                var ipAddress = ipHostInfo.AddressList[1];
+                var remoteEP = new IPEndPoint(ipAddress, port);
+
+                var client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                client.BeginConnect(remoteEP, ConnectCallback, client);
+                connectDone.WaitOne();
+
+                Send(client, Type.Login, name);
+                sendDone.WaitOne();
+
+                Receive(client);
+                receiveDone.WaitOne();
+                
+                Console.WriteLine(@"Response received : {0}", response);
+
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            return false;
+        }
+
+        private static void ConnectCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var client = (Socket) ar.AsyncState;
+
+                client.EndConnect(ar);
+
+                Console.WriteLine(@"Socket connected to {0}", client.RemoteEndPoint);
+
+                connectDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Receive(Socket client)
+        {
+            try
+            {
+                var state = new StateObject {workSocket = client};
+
+                client.BeginReceive(state.buffer, 0, StateObject.bufferSize, 0, ReceiveCallback, state);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var state = (StateObject) ar.AsyncState;
+                var client = state.workSocket;
+
+                var bytesRead = client.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                    client.BeginReceive(state.buffer, 0, StateObject.bufferSize, 0, ReceiveCallback, state);
+                }
+                else
+                {
+                    if (state.sb.Length > 1)
+                        response = state.sb.ToString();
+
+                    receiveDone.Set();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private static void Send(Socket client, Type commandType, String data)
+        {
+            data = (int)commandType + data;
+            var byteData = Encoding.ASCII.GetBytes(data);
+
+            client.BeginSend(byteData, 0, byteData.Length, 0, SendCallback, client);
+        }
+
+        private static void SendCallback(IAsyncResult ar)
+        {
+            try
+            {
+                var client = (Socket) ar.AsyncState;
+
+                var bytesSent = client.EndSend(ar);
+                Console.WriteLine(@"Sent {0} bytes to server", bytesSent);
+
+                sendDone.Set();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
