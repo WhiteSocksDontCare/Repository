1075995af79834cdb0e74ccdb6f6c909d1cc9f5e
sagawater <alexandre443@hotmail.com>
2015-04-01 12:45:17 -0400

    mvvm UI

---
+++ TP3/ChatClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.ViewModels
+{
+    class LoginViewModel : BindableBase 
+    {
+        public LoginViewModel()
+        {
+            //this.SubmitCommand = new DelegateCommand<object>(this.OnSubmit);
+            //this.QuestionnaireViewModel = new QuestionnaireViewModel();
+            //this.ResetCommand = new DelegateCommand(this.OnReset);
+        }
+    }
+}
