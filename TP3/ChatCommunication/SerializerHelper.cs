4e98fc88e88c5b5a471c7f6d767c871a49c6dadd
anthocaron <anthocaron22@gmail.com>
2015-04-01 15:48:47 -0400

    Implémentation du système de sauvegarde des listes dans des fichiers XML

--- TP3/ChatCommunication/SerializerHelper.cs
+++ TP3/ChatCommunication/SerializerHelper.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace ChatCommunication
@@ -12,39 +13,103 @@ namespace ChatCommunication
     {
         public static string Serialize<T>(this T toSerialize)
         {
+            XmlSerializer serializer = null;
+            StringWriter writer = null;
+            string serializedObject = String.Empty;
+
             if (toSerialize == null)
             {
                 return string.Empty;
             }
             try
             {
-                var serializer = new XmlSerializer(typeof(T));
-                var stringWriter = new StringWriter();
-                serializer.Serialize(stringWriter, toSerialize);
-                return stringWriter.ToString();
+                serializer = new XmlSerializer(typeof(T));
+                writer = new StringWriter();
+                serializer.Serialize(writer, toSerialize);
+                serializedObject = writer.ToString();
             }
             catch (Exception ex)
             {
-                throw new Exception("Une erreur est survenue lors de la sérialisation.", ex);
+                throw new Exception("An error occurred during serialization.", ex);
             }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+
+            return serializedObject;
         }
 
         public static T Deserialize<T>(this string toDeserialize)
         {
+            XmlSerializer serializer = null;
+            StringReader reader = null;
+            T obj = default(T);
+
             try
             {
-                XmlSerializer serializer = new XmlSerializer(typeof(T));
-                T obj;
-                using (TextReader reader = new StringReader(toDeserialize))
-                {
-                    obj = (T)serializer.Deserialize(reader);
-                }
-                return obj;
+                serializer = new XmlSerializer(typeof(T));
+                reader = new StringReader(toDeserialize);
+                obj = (T)serializer.Deserialize(reader);
             }
             catch (Exception ex)
             {
-                throw new Exception("Une erreur est survenue lors de la désérialisation.", ex);
+                throw new Exception("An error occurred during deserialization.", ex);
             }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+
+            return obj;
+        }
+        public static void SerializeToXML<T>(this T toSerialize, string fileName)
+        {
+            XmlSerializer serializer = null;
+            XmlWriter writer = null;
+
+            try
+            {
+                serializer = new XmlSerializer(typeof(T));
+                writer = XmlWriter.Create(fileName);
+                serializer.Serialize(writer, toSerialize);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred during serialization into file" + fileName + ".", ex);
+            }
+            finally
+            {
+                if (writer != null)
+                    writer.Close();
+            }
+        }
+
+        public static T DeserializeFromXML<T>(string fileName)
+        {
+            XmlSerializer serializer = null;
+            XmlReader reader = null;
+            T obj = default(T);
+
+            try
+            {
+                serializer = new XmlSerializer(typeof(T));
+                reader = XmlReader.Create(fileName);
+                obj = (T)serializer.Deserialize(reader);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("An error occurred during deserialization from file" + fileName + ".", ex);
+            }
+            finally
+            {
+                if (reader != null)
+                    reader.Close();
+            }
+
+            return obj;
         }
     }
 }
--- TP3/ChatServer/ChatServer.cs
+++ TP3/ChatServer/ChatServer.cs
@@ -6,6 +6,7 @@ using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Text;
+using System.Timers;
 
 namespace ChatServer
 {
@@ -26,9 +27,52 @@ namespace ChatServer
 
     class ChatServer
     {
+        private static double SAVE_INTERVAL = 600000;  //Intervale de temps entre deux save des listes dans le fichier XML (10 minutes)
         private static int i = 0;
         public static ManualResetEvent AllDone = new ManualResetEvent(false);
-        private static Dictionary<string, Socket> clients = new Dictionary<string,Socket>();
+        private static Dictionary<string, Socket> clients = new Dictionary<string, Socket>();
+
+        public static void LoadServerInfos()
+        {
+            //Test temporaire pour vérifier le fonctionnement des fichiers XML
+            try
+            {
+                ChatCommunication.User test = ChatCommunication.SerializerHelper.DeserializeFromXML<ChatCommunication.User>("test.xml");
+                //Console.Out.WriteLine("Deserialized : Pseudo(" + test.Pseudo + ") Password(" + test.Password + ")");
+            }
+            catch(Exception ex)
+            {
+                Console.Out.WriteLine(ex.InnerException.Message);
+            }
+        }
+        public static void ServerInfosTimer()
+        {
+            System.Timers.Timer saveTimer = new System.Timers.Timer(SAVE_INTERVAL);
+
+            saveTimer.AutoReset = true;
+            saveTimer.Elapsed += new ElapsedEventHandler(SaveServerInfos);
+
+            saveTimer.Start();
+        }
+
+        public static void SaveServerInfos(object source, ElapsedEventArgs e)
+        {
+            //Test temporaire pour vérifier le fonctionnement des fichiers XML
+            try
+            {
+                ChatCommunication.User test = new ChatCommunication.User();
+                DateTime date = DateTime.Now;
+                test.Pseudo = date.ToLongDateString();
+                test.Password = date.ToLongTimeString();
+                //Console.Out.WriteLine("Serialized : Pseudo(" + test.Pseudo + ") Password(" + test.Password + ")");
+
+                ChatCommunication.SerializerHelper.SerializeToXML(test, "test.xml");
+            }
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine(ex.InnerException.Message);
+            }
+        }
 
         public static void StartListening()
         {
