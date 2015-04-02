43ddec4d7adbe63f66b81fc8210a7178b620e9ed
Lhuky <david.lacasse1337@gmail.com>
2015-04-01 20:35:35 -0400

    ViewModel hérite de BindableBase
    ProfileViewModel
    RoomViewModel

--- TP3/ChatClient/ViewModels/ProfileViewModel.cs
+++ TP3/ChatClient/ViewModels/ProfileViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Mvvm;
 
 namespace ChatClient.ViewModels
 {
-    class ProfileViewModel
+    class ProfileViewModel : BindableBase
     {
 
     }
