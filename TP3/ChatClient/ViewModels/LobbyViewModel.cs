f4b3bfb355c2bb845003f3971e0c4da5d8c09596
Lhuky <david.lacasse1337@gmail.com>
2015-04-01 20:30:55 -0400

    Navigation de la page lobby vers les autre pages
    Aucune vérification n'est faite à ce point entre le client et le
    serveur.

--- TP3/ChatClient/ViewModels/LobbyViewModel.cs
+++ TP3/ChatClient/ViewModels/LobbyViewModel.cs
@@ -4,10 +4,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ChatCommunication;
+using Microsoft.Practices.Prism.Commands;
+using MVVM.Container;
 
 namespace ChatClient.ViewModels
 {
     class LobbyViewModel : BindableBase
     {
+        public ICommand DisconnectCommand { get; private set; }
+        public ICommand EditProfileCommand { get; private set; }
+        public ICommand LeaveRoomCommand { get; private set; }
+        public ICommand SendMessageCommand { get; private set; }
+
+        public LobbyViewModel()
+        {
+            DisconnectCommand = new DelegateCommand(Disconnect);
+            EditProfileCommand = new DelegateCommand(EditProfile);
+            LeaveRoomCommand= new DelegateCommand(LeaveRoom);
+            SendMessageCommand= new DelegateCommand(SendMessage);
+        }
+
+        public void Disconnect()
+        {
+            Container.GetA<MainViewModel>().NavigateToView(Container.GetA<LoginViewModel>());
+        }
+
+        public void EditProfile()
+        {
+            Container.GetA<MainViewModel>().NavigateToView(Container.GetA<ProfileViewModel>());
+        }
+
+        public void LeaveRoom()
+        {
+            //TODO: Cacher l'envoie de message
+        }
+
+        public void SendMessage()
+        {
+            //TODO: Envoyer le message
+        }
+
     }
 }
